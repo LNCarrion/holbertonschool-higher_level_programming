605902832e0b169c0f2d6e21e6ebc76ddd08f6c7
LNCarrion <lestycarrion@gmail.com>
2024-01-23 10:50:53 -0400

    fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,7 @@ for num in range(0, 10):
     for pos in range(0, 10):
         if num >= pos:
             pass
+        elif num == 8 and pos == 9:
+            print("{}{}".format(num, pos))
         else:
             print("{}{}".format(num, pos), end=", ")
