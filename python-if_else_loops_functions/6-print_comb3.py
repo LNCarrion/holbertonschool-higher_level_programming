1da68a8249ac182ed4d6e69773d080ca74fce312
LNCarrion <lestycarrion@gmail.com>
2024-01-23 10:33:57 -0400

    fix pycode

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for num in range(0,10):
-    for pos in range(0,10):
+for num in range(0, 10):
+    for pos in range(0, 10):
         if num >= pos:
             pass
         else:
-            print("{}{}".format(num,pos), end=", ")
+            print("{}{}".format(num, pos), end=", ")
