c126ccef71a5731f5d87cd27be1c5a8af2a18e9a
LNCarrion <lestycarrion@gmail.com>
2024-01-30 14:22:15 -0400

    best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,10 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    maxd = max(a_dictionary)
-    return maxd
+    maxd = 0
+    score = ''
+    for value in a_dictionary:
+        if a_dictionary[value] > maxd:
+            maxd = a_dictionary[value]
+            score = value
+    return score
