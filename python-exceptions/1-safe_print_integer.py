4cbdf4f631051452e5a17cf0abbc79356d15fd68
LNCarrion <lestycarrion@gmail.com>
2024-01-31 15:10:50 -0400

    fix pycode

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-      print("{:d}".format(value))
-      return True
+        print("{:d}".format(value))
+        return True
     except ValueError:
-      return False
+        return False
     except TypeError:
-      return False
+        return False
