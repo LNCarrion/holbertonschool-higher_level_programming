7ef955082e813bed40a6d85970c71988fbb43766
LNCarrion <lestycarrion@gmail.com>
2024-02-01 22:38:35 -0400

    documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
+        """
+        Initializes a Square object with a given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Returns:
+            None
+        """
         self.__size = size
