9514fba4756d5d7ad3fe58e1b75fc63334624dc2
LNCarrion <lestycarrion@gmail.com>
2024-02-02 07:53:17 -0400

    pycode fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,7 +17,7 @@ class Square:
     def size(self):
         '''get the current size'''
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -25,7 +25,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-        
+
     def area(self):
         '''Return the area of square.'''
         return (self.__size * self.__size)
