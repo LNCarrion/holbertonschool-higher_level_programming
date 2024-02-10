ae3148b1f68eaf7c8b1dfea77905efae28cf9b98
LNCarrion <lestycarrion@gmail.com>
2024-02-10 17:40:47 -0400

    public instance method

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creating a class"""
+
+
+class BaseGeometry:
+    '''
+    public instance method
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
