c35796ae5c7aab6ad58aeaff1da8a9089469c8cb
LNCarrion <lestycarrion@gmail.com>
2024-02-10 17:52:56 -0400

    base geometry

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""creating a class"""
+
+
+class BaseGeometry:
+    '''
+    public instance method
+    '''
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError(f"{name} must be an ineger")
+        if value <= 0:
+            raise ValueError (f"{name} must be greater than 0")
