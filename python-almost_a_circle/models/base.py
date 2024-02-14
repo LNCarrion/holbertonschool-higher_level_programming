95725e602c1cf26b6f8d7748e995ed1323c07f3f
LNCarrion <lestycarrion@gmail.com>
2024-02-13 23:27:15 -0400

    base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class base"""
+
+class Base():
+    """private atribute"""
+    __nb_objects = 0
+    """init the class"""
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects 
