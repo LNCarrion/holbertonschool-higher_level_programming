3c5b1c2ba6c1984ed846d786b3497cc34f042053
LNCarrion <lestycarrion@gmail.com>
2024-02-14 02:20:34 -0400

    fixing setters, and error message

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -37,7 +37,7 @@ class Rectangle(Base):
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
 
@@ -45,7 +45,7 @@ class Rectangle(Base):
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if value < 0:
+        if value <=0:
             raise ValueError("height must be > 0")
         self.__height = value
 
@@ -54,7 +54,7 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("x must be an integer")
         if value < 0:
-            raise ValueError("x must be > 0")
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @y.setter
@@ -62,5 +62,5 @@ class Rectangle(Base):
         if type(value) is not int:
             raise TypeError("y must be an integer")
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
