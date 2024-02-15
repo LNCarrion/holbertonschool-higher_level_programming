2194bfe1bacbe11b0c49da40eca1a07791cc638d
LNCarrion <lestycarrion@gmail.com>
2024-02-15 17:57:26 -0400

    update function: update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -88,15 +88,8 @@ class Rectangle(Base):
         string += f" - {self.__width}/{self.__height}"
         return string
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """assing an argument to an atribute"""
-        if len(args) > 0:
-            self.id = args[0]
-        if len(args) > 1:
-            self.width = args[1]
-        if len(args) > 2:
-            self.height = args[2]
-        if len(args) > 3:
-            self.x = args[3]
-        if len(args) > 4:
-            self.y = args[4]
+        for key, value in kwargs.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
