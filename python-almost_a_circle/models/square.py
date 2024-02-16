27a5c4e6b83c22ce9cca4c58e3541e4a0cd4dd2c
LNCarrion <lestycarrion@gmail.com>
2024-02-15 20:15:29 -0400

    new subclass:square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""square class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    def __init__(sel, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """return the info of the class"""
+        string = f"[Square] ({self.id})"
+        string += f" {self.x}/{self.y} - {self.height}"
+        return string
