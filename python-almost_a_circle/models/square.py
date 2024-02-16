ef876cebe94b5202a2a9cf72c943b9f9e6a24e2f
LNCarrion <lestycarrion@gmail.com>
2024-02-16 14:07:14 -0400

    adding fuction: dictionary

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -38,3 +38,7 @@ class Square(Rectangle):
             self.x = args[2]
         if len(args) == 4:
             self.y = args[3]
+
+    def to_dictionary(self):
+        dict = {'id': self.id, 'x': self.x,
+                'size':self.size, 'y': self.y}
