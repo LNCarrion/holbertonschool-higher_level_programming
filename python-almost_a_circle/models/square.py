9b9623de275ab2cac99adfd99b6b428b00e93e30
LNCarrion <lestycarrion@gmail.com>
2024-02-15 22:45:17 -0400

    adding setter and getter

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,17 @@ class Square(Rectangle):
     def __init__(sel, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    """getter"""
+    @property
+    def size(self):
+        return self.width
+
+    """setter"""
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """return the info of the class"""
         string = f"[Square] ({self.id})"
