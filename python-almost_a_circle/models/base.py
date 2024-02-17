863ab3eaecec258e8bebafa7496baecc44a7cd87
LNCarrion <lestycarrion@gmail.com>
2024-02-17 12:39:12 -0400

    load from file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -51,3 +51,19 @@ class Base():
 
         cls.update(dummy, **dictionary)
         return dummy
+
+    """load classes from a json file"""
+    @classmethod
+    def load_from_file(cls):
+        """checks if the .json"""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, 'r') as file:
+                string = file.read()
+                class_dict = cls.from_json_string(string)
+            class_list = []
+            for i in class_dict:
+                class_list.append(cls.create(**i))
+            return class_list
+        except FileNotFoundError():
+            return []
