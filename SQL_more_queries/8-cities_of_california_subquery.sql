5a060dcaa91ced6be057f649c715f6e836d35b4e
LNCarrion <lestycarrion@gmail.com>
2024-03-15 22:47:20 -0400

    fix

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
 -- adding all cities of california
-SELECT *
-FROM cities
-Where state_id = (SELECT iD FROM WHERE name = 'california')
-ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id = (SELECT id FROM states WHERE name = "California")
+ORDER BY id;
