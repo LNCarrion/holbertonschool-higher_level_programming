a4217b3d3475124b6961a27f0624fd0045c82a14
LNCarrion <lestycarrion@gmail.com>
2024-03-22 16:19:20 -0400

    adding comment

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-
+    """list all states"""
     db = MySQLdb.Connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
