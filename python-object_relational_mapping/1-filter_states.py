8399253a9c0bbe7082b68991c9f27c2a5596c9b6
LNCarrion <lestycarrion@gmail.com>
2024-03-22 16:16:25 -0400

    fix 2

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,8 +5,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    if __name__ == "__main__":
-
     db = MySQLdb.Connect(host="localhost", port=3306,
                          user="root", passwd=" ", db="hbtn_0e_0_usa")
     cursor = db.cursor()
